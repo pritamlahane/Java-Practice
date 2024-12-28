39a52e5c205fcb76c00890c1d23cb9d70248f09a
pritam <your.email@example.com>
2024-12-28 17:38:42 +0530

    first loop

---
+++ src/loops02/Loop01.java
@@ -0,0 +1,10 @@
+package loops02;
+
+public class Loop01 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Loop finished");
+    }
+}
