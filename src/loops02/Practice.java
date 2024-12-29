1c928646075e7d423878ff74f5a8f9e693e3507b
pritam <your.email@example.com>
2024-12-29 07:56:10 +0530

    finally block use cases

--- src/loops02/Practice.java
+++ src/loops02/Practice.java
@@ -1,19 +1,25 @@
 package loops02;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Practice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a Number :");
-		int n = sc.nextInt();
-		
-		for(int i = 1; i<=10; i++) {
-			System.out.println(n + "X" +i+ "=" + n*i );
-		}
-		
+//
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("Enter a Number :");
+//		int n = sc.nextInt();
+//
+//		for(int i = 1; i<=10; i++) {
+//			System.out.println(n + "X" +i+ "=" + n*i );
+//		}
+//
+
+
 	}
 
 }
+
+
